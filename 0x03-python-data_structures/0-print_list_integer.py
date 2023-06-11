c0a511a0b468a86a2aad1e931bf8647bae2a6159
reem <tarekreem950@gmail.com>
2023-06-11 09:49:45 -0700

    task0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """print list
+    Args:
+        my_list: list
+    Returns:
+        nothing
+    """
+    i = 0
+    while i < len(my_list):
+        print("{}".format(my_list[i]))
+        i = i + 1
