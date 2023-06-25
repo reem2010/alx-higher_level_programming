0864ffba5861d3890a4e23a2a8f17edf6d1ac8e3
reem <tarekreem950@gmail.com>
2023-06-25 11:08:21 -0700

    print mattrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,8 +9,8 @@ def print_matrix_integer(matrix=[[]]):
         j = 0
         while j < len(matrix[i]):
             if j != len(matrix[i]) - 1:
-                print("{} ".format(matrix[i][j]), end="")
+                print("{:d} ".format(matrix[i][j]), end="")
             else:
-                print("{}".format(matrix[i][j]))
+                print("{:d}".format(matrix[i][j]))
             j = j + 1
         i = i + 1
