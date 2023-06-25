6389b104458ded0d835a77eff2653dbbef99200a
reem <tarekreem950@gmail.com>
2023-06-25 11:07:19 -0700

    print mattrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-Prototype: def print_matrix_integer(matrix=[[]]):
+def print_matrix_integer(matrix=[[]]):
     """print matrix
     Args:
         matrix: list of lists
     """
     i = 0
-    j = 0
     while i < len(matrix):
+        j = 0
         while j < len(matrix[i]):
-            print(
+            if j != len(matrix[i]) - 1:
+                print("{} ".format(matrix[i][j]), end="")
+            else:
+                print("{}".format(matrix[i][j]))
+            j = j + 1
+        i = i + 1
