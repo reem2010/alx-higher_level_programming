4a6f738461bbc59017f6ad1df3ead100211248dd
reem <tarekreem950@gmail.com>
2023-06-25 15:04:56 -0700

    square matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+    for i in range(len(new_matrix)):
+        new_matrix [i] = list((map(lambda y : y ** 2, new_matrix[i])))
+    return (new_matrix)
+
