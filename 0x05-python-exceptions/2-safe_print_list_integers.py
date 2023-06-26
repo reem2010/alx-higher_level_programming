41e8a14097007ad905b1f83618cdbbcf08788ca7
reem <tarekreem950@gmail.com>
2023-06-26 06:46:14 -0700

    safe print list integer

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    if x == 0:
+        print("")
+        return (0)
+    l = 0;
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            l = l + 1
+        except TypeError:
+            continue
+        except ValueError:
+            continue
+    print("")
+    return (l)
