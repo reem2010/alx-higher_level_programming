4555cf803fa958a2b859e0d53d6147d227c64e54
reem <tarekreem950@gmail.com>
2023-06-25 17:12:00 -0700

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return  None
+        return (None)
     b = list(a_dictionary.values())
     b.sort()
     for i in a_dictionary.keys():
         if a_dictionary[i] == b[-1]:
-            return i
+            return (i)
