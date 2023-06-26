822d8051a878fcdffae593afc64da8d887852b30
reem <tarekreem950@gmail.com>
2023-06-26 07:51:39 -0700

    integer print with error message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except TypeError as msg:
+        print("Exception: {}".format(msg), file=sys.stderr)
+        return (False)
+    except ValueError as msg:
+        print("Exception: {}".format(msg), file=sys.stderr)
+        return (False)
