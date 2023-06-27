c6ee5d16b5860b00e6e80e008717d003cd6beedb
reem <tarekreem950@gmail.com>
2023-06-27 06:30:45 -0700

    task0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """square class
-
-    this is empty square class
-
-    Attributes:
-        no attributes
-    Methods:
-        no methods
-    """
+    """square class"""
     pass
