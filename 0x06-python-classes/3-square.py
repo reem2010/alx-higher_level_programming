65bc2aabc2a6cf215e9d919df702647de25753c9
reem <tarekreem950@gmail.com>
2023-06-27 07:26:23 -0700

    square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""this file contain the square class"""
+
+
+class Square:
+    """square class whth no arguments"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """calculate the area
+        Return:
+            the area
+        """
+        return ((self.__size) ** 2)
+    pass
