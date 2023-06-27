f24e541aa0eba8737f689542ef3fb8d3653a04e6
reem <tarekreem950@gmail.com>
2023-06-27 07:06:40 -0700

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     """square class whth no arguments"""
     def __init__(self, size=0):
         try:
-            if size < 0:
+            if int(size) < 0:
                 raise ValueError("size must be >= 0")
         except TypeError:
             raise TypeError("size must be an integer")
--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -27,7 +27,7 @@ except Exception as e:
     print(e)
 
 try:
-    my_square_4 = Square(-89)
+    my_square_4 = Square(None)
     print(type(my_square_4))
     print(my_square_4.__dict__)
 except Exception as e:
