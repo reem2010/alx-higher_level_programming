09687e9a6d7f380c7e4de694c368e5c5627afda2
reem <tarekreem950@gmail.com>
2023-06-27 06:51:16 -0700

    task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this file contain the square class"""
+
+
+class Square:
+    """square class whth no arguments"""
+    def __init__(self, __size):
+        self.__size = __size
+        pass
+    pass
