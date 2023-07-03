0c885bf7811997ad36147e3f9275a13a76ca8659
reem <tarekreem950@gmail.com>
2023-07-03 10:24:07 -0700

    task1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/pythion3
 """this module define rectangle class"""
 
 
 class Rectangle:
     """rectangle class"""
     def __init__(self, width=0, height=0):
-         self.width = width
-         self.height = height
+        self.width = width
+        self.height = height
+
     @property
     def width(self):
         return self.__width
