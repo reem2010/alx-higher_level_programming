93768af689d3ec285f3e42f1df1587aee10a87a9
reem <tarekreem950@gmail.com>
2023-07-05 14:26:26 -0700

    task1

--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -2,4 +2,7 @@
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 matrix = [[3]]
-print(matrix_divided(matrix, 3))
+try:
+    print(matrix_divided([[3, 9], [12, 15]], 0))
+except Exception as e:
+    print(e)
