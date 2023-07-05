3957729fe9091d09a654c5a189c03f76a1968825
reem <tarekreem950@gmail.com>
2023-07-05 12:23:21 -0700

    task 0

--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-matrix_divided = __import__('2-matrix_divided').matrix_divided
+add_integer = __import__('0-add_integer').add_integer
 
-matrix = ["re"]
+print(add_integer(1, 2))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(None, -2))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
 
-
-
-print(matrix_divided(matrix, 3))
-print(matrix)
