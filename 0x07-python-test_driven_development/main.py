cf39e6ecc63f0692cba5ed48ede518778a4e4b60
reem <tarekreem950@gmail.com>
2023-07-05 12:32:19 -0700

    task0

--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -2,9 +2,8 @@
 add_integer = __import__('0-add_integer').add_integer
 
 print(add_integer(1, 2))
-print(add_integer(100, -2))
+print(add_integer())
 print(add_integer(2))
-print(add_integer(None, -2))
 try:
     print(add_integer(4, "School"))
 except Exception as e:
