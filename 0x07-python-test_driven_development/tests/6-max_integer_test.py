1c35695d7484563f4da9a2f35c64d61550a3f7e6
reem <tarekreem950@gmail.com>
2023-07-07 15:19:02 -0700

    task6

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,7 @@
+>>> max_integer = __import__('6-max_integer').max_integer
+>>> print(max_integer([1, 2, 3, 4]))
+4
+>>> print(max_integer([5, 1, 2, 3, 4]))
+5
+>>> print(max_integer([1, 3, 5, 4, 2]))
+5
