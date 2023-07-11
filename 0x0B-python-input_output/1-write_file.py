ab24d5d843ba72b22589371bbb63bc7aa16ae8f8
reem <tarekreem950@gmail.com>
2023-07-11 16:59:53 -0700

    write to file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""in this there is a write function"""
+
+def write_file(filename="", text=""):
+    """write function"""
+    with open(filename, "w") as file:
+        file.write(text)
+        return (file.tell())
