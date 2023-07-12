a37c7d0587eef303ff220f4d925ca9f2b37f0856
reem <tarekreem950@gmail.com>
2023-07-11 17:59:28 -0700

    From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""in this module a function that
+returns an object"""
+import json
+
+
+def from_json_string(my_str):
+    """ function that returns an object"""
+    return (json.loads(my_str))
+
+
