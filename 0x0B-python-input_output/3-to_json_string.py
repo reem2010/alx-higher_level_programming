ec4a7db1a0c440bcb6154475679ccdbaffb10ea6
reem <tarekreem950@gmail.com>
2023-07-11 17:41:30 -0700

    JSON representatio

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""in tis module a function that returns
+the JSON representation of an object (string)"""
+import json
+
+
+def to_json_string(my_obj):
+    """this function returns
+    the JSON representation of an object (string)"""
+    return (json.dumps(my_obj))
