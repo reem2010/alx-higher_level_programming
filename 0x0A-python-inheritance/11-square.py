b5fd1df5f8409d2799cd36f9331d0ddf84024228
reem <tarekreem950@gmail.com>
2023-07-12 12:31:31 -0700

    square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square mpdule"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        return (f"[Rectangle] {self.__size}/{self.__size}")
