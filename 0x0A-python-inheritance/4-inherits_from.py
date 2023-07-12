e3d45a1f203cb2cc81b81dddab3df475b177125b
reem <tarekreem950@gmail.com>
2023-07-12 10:07:30 -0700

    Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function checks if
+he object is an instance of a class that inherited
+from the specified class"""
+
+
+def inherits_from(obj, a_class):
+    """function checks if
+he object is an instance of a class that inherited
+from the specified class"""
+    return (isinstance(obj, a_class))
