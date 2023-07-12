0b2ed55ca1271e4e023c6e2df8f71a6dce4e92c8
reem <tarekreem950@gmail.com>
2023-07-12 09:39:37 -0700

    Exact same objectwq

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function checks if
+the object is exactly an instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """function checks if
+the object is exactly an instance of the specified class"""
+    return (isinstance(obj, a_class))
