4df816e12181294423ca62471875ac26ae081f29
reem <tarekreem950@gmail.com>
2023-07-12 08:54:14 -0700

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class MyList that inherits from list"""
+
+
+class MyList(list):
+    """class inherits from list"""
+    def __init__(self):
+        pass
+
+    def print_sorted(self):
+        return(self.sort())
+
