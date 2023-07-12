279534e05bc97a698400068f81c65cfa267bb92f
reem <tarekreem950@gmail.com>
2023-07-12 11:35:07 -0700

    rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """Geometry class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if (type(value) != int):
+            raise TypeError(f"{name} must be an integer")
+        if (value <= 0):
+            raise ValueError(f"{name} must be greater than 0")
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
