77fbbc0b780aafa097d010fd28fb354046dc0b2c
reem <tarekreem950@gmail.com>
2023-07-12 15:00:56 -0700

    Load, add, savee

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,8 +6,6 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-file = open("add_item.json", "w")
-file.close()
 lis = load_from_json_file("add_item.json")
 for i in range(len(sys.argv)):
     if i == 0:
