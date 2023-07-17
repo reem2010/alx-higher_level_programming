789ee2fddef762f5b0d0199eaf7840148450aefd
reem <tarekreem950@gmail.com>
2023-07-17 08:20:04 -0700

    base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """Dictionary to JSON string"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return ("[]")
         return (json.dumps(list_dictionaries))
