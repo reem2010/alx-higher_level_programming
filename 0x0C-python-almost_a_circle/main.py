fdc8287ee17f35b704448a8474973dad6823f05c
reem <tarekreem950@gmail.com>
2023-07-17 11:38:32 -0700

    rectangle class

--- 0x0C-python-almost_a_circle/main.py
+++ 0x0C-python-almost_a_circle/main.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
-""" 17-main """
+""" Doc """
 from models.rectangle import Rectangle
 
 if __name__ == "__main__":
 
-    r1 = Rectangle(3, 5, 1)
-    r1_dictionary = r1.to_dictionary()
-    r2 = Rectangle.create(**r1_dictionary)
+    r1 = Rectangle(10, 10, 10, 10)
     print(r1)
-    print(r2)
-    print(r1 is r2)
-    print(r1 == r2)
+
+    r1.update(89)
+    print(r1)
+
+    r1.update(89, 2)
+    print(r1)
+
+    r1.update(89, 2, 3)
+    print(r1)
+
+    r1.update(89, 2, 3, 4)
+    print(r1)
+
+    r1.update(89, 2, 3, 4, 5)
+    print(r1)
