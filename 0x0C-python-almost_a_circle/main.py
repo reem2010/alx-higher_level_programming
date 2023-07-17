85df64963d0633719e85b7d89088d9e8f9610a23
reem <tarekreem950@gmail.com>
2023-07-17 08:16:19 -0700

    base class

--- 0x0C-python-almost_a_circle/main.py
+++ 0x0C-python-almost_a_circle/main.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-""" 12-main """
+""" 15-main """
 from models.rectangle import Rectangle
+from models.square import Square
 
 if __name__ == "__main__":
 
-    r1 = Rectangle(10, 2, 1, 9)
-    print(r1)
-    r1_dictionary = r1.to_dictionary()
-    print(r1_dictionary)
-    print(type(r1_dictionary))
+    r1 = Square(7, 7, 2, 8)
+    r2 = Square(2, 2)
+    Square.save_to_file([r1, r2])
 
-    r2 = Rectangle(1, 1)
-    print(r2)
-    r2.update(**r1_dictionary)
-    print(r2)
-    print(r1 == r2)
+    with open("Square.json", "r") as file:
+        print(file.read())
--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """base module"""
+import json
 
 
 class Base:
@@ -7,7 +8,6 @@ class Base:
 
 
     __nb_objects = 0
-
     def __init__(self, id=None):
         if id is not None:
             self.id = id
@@ -15,3 +15,28 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return ("[]")
+        return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        from models.rectangle import Rectangle
+        out = []
+        if cls is Rectangle:
+            name = "Rectangle.json"
+        else:
+            name = "Square.json"
+        if ((list_objs) is not None):
+            for obj in list_objs:
+                out.append(obj.to_dictionary())
+            out = cls.to_json_string(out)
+        with open(name, "w") as file:
+            file.write(out)
+
+    @classmethod
+    def from_json_string(json_string):
+        out = []
+        if json_string is not None:
+            return
