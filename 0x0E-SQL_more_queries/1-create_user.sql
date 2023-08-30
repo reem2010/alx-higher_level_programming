6c4960426feaf0f46ef4a01edaf57ea211568f3f
reem <tarekreem950@gmail.com>
2023-08-30 13:30:19 -0700

    create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- creates the MySQL server user
+CREATE USER if not EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
