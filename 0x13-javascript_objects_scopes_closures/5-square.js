9fe17cac002fd560db74c771fbd90e97c4358cf1
reem <tarekreem950@gmail.com>
2023-09-12 12:53:52 -0700

    square

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super();
+    this.height = size;
+    this.width = size;
+  }
+}
+module.exports = Square;
