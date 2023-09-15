d1be39ce3234679aa39db7305ced6199ec25eda9
reem <tarekreem950@gmail.com>
2023-09-15 08:15:31 -0700

    SQL Injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N"""
+from sys import argv
+import MySQLdb
+if __name__ == '__main__':
+    host = "localhost"
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    port = 3306
+    db = MySQLdb.connect(host, username, password, db_name, port)
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY\
+            %s ORDER BY states.id ASC"
+    cursor.execute(query, (argv[4],))
+    data = cursor.fetchall()
+    for i in data:
+        print(i)
+    cursor.close()
+    db.close()
