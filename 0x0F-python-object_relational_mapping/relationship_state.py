3cfd10b5efd8a3faccf32be9c431e661d3142ab4
reem <tarekreem950@gmail.com>
2023-09-16 10:08:28 -0700

    City relationship

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -12,4 +12,4 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128))
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    cities = relationship("City", backref="state")
