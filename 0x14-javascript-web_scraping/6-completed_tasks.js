e3a831347713b69b17c836f0d6c47e375d0c8c7a
reem <tarekreem950@gmail.com>
2023-10-24 18:53:57 -0700

    id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,14 +5,13 @@ request(process.argv[2], function (error, response, body) {
     console.error(error);
     return;
   }
-  let res = {};
+  const res = {};
   const tasks = JSON.parse(response.body);
   for (let i = 0; i < tasks.length; i++) {
-    if (!res[tasks[i].userId]){
-      res[tasks[i].userId] = 0;
-    }
     if (tasks[i].completed) {
-      res[tasks[i].userId] = res[tasks[i].userId] + 1 || 1;
+      if (!res[tasks[i].userId]) {
+        res[tasks[i].userId] = 1;
+      } else { res[tasks[i].userId] = res[tasks[i].userId] + 1; }
     }
   }
   console.log(res);
