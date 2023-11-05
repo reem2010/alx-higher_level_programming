9e12e7ad283132dcae913cd36e77ff0fbedffacc
reem <tarekreem950@gmail.com>
2023-11-05 02:00:01 +0200

    movies title

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  const movies = data.results;
+  for (let i = 0; i < movies.length; i++) {
+    $('UL#list_movies').append('<li>' + movies[i].title + '</li>');
+  }
+});
